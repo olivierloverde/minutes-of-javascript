d670e5d64a8898033784eb92fe213ec9a35b79d7
Antonio Piras <antopiras89@gmail.com>
2021-04-03 17:16:01 +0200

    Update snippets/24-use-switch-case-with-numeric-ranges.js
    Co-authored-by: Olivier <olivier.loverde@gmail.com>

--- snippets/24-use-switch-case-with-numeric-ranges.js
+++ snippets/24-use-switch-case-with-numeric-ranges.js
@@ -1,6 +1,10 @@
 // Minutes of Javascript #24 - Use switch/case statement with numeric ranges
 
-//You can use a switch statement with a numeric range in the case with:
+// switch (true) may seem absurd but you can match against values as well as expressions. 
+// Expressions will be evaluated before matching, so any expression that evals to true will be matched.
+// Ref: https://262.ecma-international.org/10.0/#sec-switch-statement
+
+// For example, you can use a switch statement with a numeric range in the case with:
 function canDrink(age) {
     switch (true) {
         case (age < 18):
